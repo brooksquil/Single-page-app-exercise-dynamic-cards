d0b6f80cb869a3745783d1c24fef078a80a7184b
Brooke Wittenberg <brooke.wittenberg@gmail.com>
2017-08-07 15:53:09 -0500

    finished js, started css, may come back and add more styling

--- cards.js
+++ cards.js
@@ -1,24 +1,43 @@
 console.log("Time for Dynamic Cards");
 
 
-/* <div id="input-container">
-     <input type="text" id="input-text">
-        <button type="submit" id="btn-create">Create</button>
-        </div id="output-container">
-    <h4>Text Here</h4>
-    <output name="card-display" id="card-output"></output> */
-var makeCards = function() {
-    var cards = [];
-    console.log("makeCards", makeCards);
 
-    this.getCards = function() {
-        return cards;
-    }
-    this.addCards = function(item) {
-        cards.push(item);
-    }
-    this.addAndRetrieve = function(item) {
-        cards.push(item);
-        return items;
-    }
-}();
+
+//connect to DOM:
+let input = document.getElementById("input-text");
+let createBtn = document.getElementById("btn-create");
+let cardOutput = document.getElementById("card-output");
+
+//add event listener:
+createBtn.addEventListener("click", makeCard);
+
+//create makeCard function:
+function makeCard() {
+    //get text from input using .value
+    let userInput = input.value;
+    //create div element for HTML
+    let dynamicCard = document.createElement("div");
+    //format card
+    dynamicCard.innerHTML = `<div id="cards">
+    <h3 id="userToDo">${userInput}</h3>
+    </div>`;
+    //append output to card
+    cardOutput.appendChild(dynamicCard);
+
+    //create delete button
+    let deleteBtn = document.createElement("BUTTON");
+    //create text node for delete button
+    let deleteBtntext = document.createTextNode("Task Complete");
+    //appened button text to button
+    deleteBtn.appendChild(deleteBtntext);
+    //append button to card
+    dynamicCard.appendChild(deleteBtn);
+    //delete button event listener
+    deleteBtn.addEventListener("click", deleteCard);
+    //delete function to remove card from DOM, deletes child node
+    function deleteCard() {
+        let card = this.parentNode;
+        cardOutput.removeChild(card);
+    };
+
+};
